96bd34fc7e81423db3ecdaf2b562cf09875b4060
mandu <jys0972@gmail.com>
2025-01-08 14:43:08 +0900

    Initialize Spring Boot

---
+++ src/test/java/com/dudoji/dudoji/DudojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudoji.dudoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DudojiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
