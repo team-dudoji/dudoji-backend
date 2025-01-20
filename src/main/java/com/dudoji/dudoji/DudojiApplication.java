bbbc4e2de2dd349eaaa51ef6489a4e3d2d01ff45
mandu <jys0972@gmail.com>
2025-01-20 15:09:30 +0900

    Revert "Init Project"
    This reverts commit 4bbed1293d9f6d9fcfec2c91475e9d9a38fb22a3.

---
+++ src/main/java/com/dudoji/dudoji/DudojiApplication.java
@@ -0,0 +1,13 @@
+package com.dudoji.dudoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DudojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DudojiApplication.class, args);
+    }
+
+}
