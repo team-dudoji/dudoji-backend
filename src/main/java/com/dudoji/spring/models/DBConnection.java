a8bd5458bb3e79687a7ffab714430b43c764c68b
mandu <jys0972@gmail.com>
2025-01-20 16:21:49 +0900

    feat(DBConnection.java) : static에서 Bean으로 관리하도록 수정, Dotenv 연결

--- src/main/java/com/dudoji/spring/models/DBConnection.java
+++ src/main/java/com/dudoji/spring/models/DBConnection.java
@@ -1,5 +1,7 @@
 package com.dudoji.spring.models;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +12,20 @@ import java.sql.SQLException;
 @Component
 public class DBConnection {
 
-    @Value("${spring.datasource.username}")
-    private static String username;
-    @Value("${spring.datasource.password}")
-    private static String password;
-    @Value("${spring.datasource.url}")
-    private static String url;
-    @Value("${spring.datasource.driver-class-name}")
-    private static String driverClassName;
+    @Autowired
+    private Dotenv dotenv;
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        dotenv.load();
 
         Connection con = null;
 
