e132bce6c8b933582a1df487fe87805dfd52fdd5
mandu <jys0972@gmail.com>
2025-01-20 17:56:25 +0900

    feature(DudojiApplicationTests.java) : MapSection 빌더 테스팅 추가

--- src/test/java/com/dudoji/spring/DudojiApplicationTests.java
+++ src/test/java/com/dudoji/spring/DudojiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.dudoji.spring;
 
 import com.dudoji.spring.models.DBConnection;
+import com.dudoji.spring.models.domain.MapSection;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,6 @@ class DudojiApplicationTests {
 
     @Test
     void testDBConnection() {
-
         try (Connection connection = dbConnection.getConnection()) {
             assertNotNull(connection, "Database Connection should not be null");
             System.out.println("Database connection successful");
@@ -27,4 +27,20 @@ class DudojiApplicationTests {
         }
     }
 
+    @Test
+    void testMapSectionCreating() {
+        MapSection mapSection = new MapSection.Builder()
+                .setUid(1)
+                .setX(0)
+                .setY(0)
+                .build();
+        assertNotNull(mapSection, "MapSection created Successful");
+        MapSection detailMapSection = new MapSection.Builder()
+                .setUid(1)
+                .setX(0)
+                .setY(0)
+                .setBitmap(new byte[] {1, 2, 3})
+                .build();
+        assertNotNull(detailMapSection, "DetailMapSection created Successful");
+    }
 }
