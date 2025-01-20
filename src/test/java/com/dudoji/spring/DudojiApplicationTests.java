67ad0380d1635dc415794490c13077e59e8bb428
mandu <jys0972@gmail.com>
2025-01-20 16:26:17 +0900

    feat(DudojiApplicationTests.java) : DBConnection 연결 테스트 구현

--- src/test/java/com/dudoji/spring/DudojiApplicationTests.java
+++ src/test/java/com/dudoji/spring/DudojiApplicationTests.java
@@ -2,22 +2,29 @@ package com.dudoji.spring;
 
 import com.dudoji.spring.models.DBConnection;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DudojiApplicationTests {
 
+    @Autowired
+    DBConnection dbConnection;
 
     @Test
     void testDBConnection() {
-        try{
-            DBConnection.getConnection();
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
+        try (Connection connection = dbConnection.getConnection()) {
+            assertNotNull(connection, "Database Connection should not be null");
+            System.out.println("Database connection successful");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("testDBConnection: Fail Tast");
+        }
     }
 
 }
