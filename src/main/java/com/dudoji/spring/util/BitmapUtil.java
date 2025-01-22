b3572afec5226a82d17e22ca00dc79fbbbad17fc
mandu <jys0972@gmail.com>
2025-01-22 18:12:42 +0900

    doc(BitmapUtil.java) : To Do 설명 추가 Function Signniture 추가

--- src/main/java/com/dudoji/spring/util/BitmapUtil.java
+++ src/main/java/com/dudoji/spring/util/BitmapUtil.java
@@ -2,12 +2,23 @@ package com.dudoji.spring.util;
 
 public class BitmapUtil {
 
-    public byte[] setBit(byte[] bitmap, int baseLng, int baseLat, int lng, int lat) {
+    public static byte[] setBit(byte[] bitmap, int baseLng, int baseLat, int lng, int lat) {
         // TODO
+        // 위도 경도에 맞는 위치의 bit를 1로 바꾸는 함수
+        // return은 bitmap
         return null;
     }
-    public byte[] setCloseBits(byte[] bitmap, int baseLng, int baseLat, int lng, int lat) {
+    public static byte[] setCloseBits(byte[] bitmap, int baseLng, int baseLat, int lng, int lat, float radius) {
         // TODO
+        // 위도 경도에 맞는 위치의 bit들을 1로 바꾸는 함수 (원 모양)
+        // return은 bitmap
         return null;
+    }
+
+    public static boolean isExplored(byte[] bitmap) {
+        // TODO
+        // 80 이상 탐험 됐는지 체크
+        // 함수명은 좀 더 General하게 바꾸는게 좋을듯
+        return false;
     }
 }
