5bd634a93656243aeed55faf45f099144682b524
mandu <jys0972@gmail.com>
2025-02-17 16:43:56 +0900

    feature(RevealCirclesRequestDto.java) : Reveal Circle로 네이밍 변경, toString 구현

--- src/main/java/com/dudoji/spring/dto/RevealCirclesRequestDto.java
+++ src/main/java/com/dudoji/spring/dto/RevealCirclesRequestDto.java
@@ -8,13 +8,27 @@ import java.util.List;
 @Getter
 @Setter
 public class RevealCirclesRequestDto {
-    List<PositionDto> positions;
+    List<RevealCircleDto> revealCircles;
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (RevealCircleDto circleDto : revealCircles) {
+            builder.append("{").append(circleDto.toString()).append("} ");
+        }
+        return builder.toString();
+    }
 
     @Setter
     @Getter
-    public static class PositionDto {
+    public static class RevealCircleDto {
         private double lat;
         private double lng;
         private double radius;
+
+        @Override
+        public String toString() {
+            return String.format("lat: %.2f, lng: %.2f, radius: %.2f", lat, lng, radius);
+        }
     }
 }
