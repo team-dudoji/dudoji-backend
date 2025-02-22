0d2dfe105fd4b93ddbf976fcba8774913f63a4a6
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-22 19:02:45 +0900

    feature(UserDao.java) : 카카오 id 관련 쿼리문 구현 및 메소드 구현

--- src/main/java/com/dudoji/spring/models/dao/UserDao.java
+++ src/main/java/com/dudoji/spring/models/dao/UserDao.java
@@ -14,11 +14,15 @@ public class UserDao {
     private DBConnection dbConnection;
 
     private static String GET_USER_BY_ID =
-            "select name, email, created_at from \"User\" where id=?";
+            "select name, email, created_at, kakao_id from \"User\" where id=?";
     private static String REMOVE_USER_BY_ID =
             "delete from \"User\" where id=?";
     private static String CREATE_USER_BY_ID =
             "insert into \"User\"(name, email) values (?, ?) returning id";
+    private static String GET_USER_BY_KAKAO_ID =
+            "SELECT id, name, email, created_at FROM \"User\" WHERE kakao_id=?";
+    private static String CREATE_USER_BY_KAKAO_ID =
+            "insert into \"User\"(name, email, kakao_id) values (?, ?, ?) returning id";
 
     public User getUserById(long uid) {
         try (Connection connection  = dbConnection.getConnection()) {
@@ -29,7 +33,26 @@ public class UserDao {
                 String name = resultSet.getString(1);
                 String email = resultSet.getString(2);
                 Timestamp createdAt = resultSet.getTimestamp(3);
-                return new User(uid, name, email, createdAt);
+                long kakaoUserId = resultSet.getLong(4);
+                return new User(uid, name, email, createdAt, kakaoUserId);
+            }
+            return null;
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User getUserByKakaoId(long kakaoId) {
+        try (Connection connection = dbConnection.getConnection()) {
+            PreparedStatement preparedStatement =  connection.prepareStatement(GET_USER_BY_KAKAO_ID);
+            preparedStatement.setLong(1, kakaoId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                long uid = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String email = resultSet.getString(3);
+                Timestamp createdAt = resultSet.getTimestamp(4);
+                return new User(uid, name, email, createdAt, kakaoId);
             }
             return null;
         } catch (SQLException | ClassNotFoundException e) {
@@ -61,5 +84,22 @@ public class UserDao {
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public long createUserWithKakaoId(String name, String email, long kakaoId) {
+        try (Connection connection  = dbConnection.getConnection()) {
+            PreparedStatement preparedStatement =  connection.prepareStatement(CREATE_USER_BY_KAKAO_ID);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setLong(3, kakaoId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                long uid = resultSet.getLong("id");
+                return uid;
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return -1;
     }
 }
