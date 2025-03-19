e60852129bfa9a608dcd2960f80ea4fc32bbff2c
Vackam <53655740+Vackam@users.noreply.github.com>
2025-03-19 20:25:20 +0900

    feature(JoinController.java) : passwordEncoder 변경으로 인한 수정

--- src/main/java/com/dudoji/spring/controller/JoinController.java
+++ src/main/java/com/dudoji/spring/controller/JoinController.java
@@ -4,7 +4,7 @@ import com.dudoji.spring.models.dao.UserDao;
 import com.dudoji.spring.models.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JoinController {
 
     private final UserDao userDao;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/joinForm")
     public String joinForm() {
@@ -29,12 +29,11 @@ public class JoinController {
     public String join(User user) {
         log.info("join post approach");
 
-        user.setRole("user");
         String rawPassword = user.getPassword();
-        String encodedPassword = bCryptPasswordEncoder.encode(rawPassword);
+        String encodedPassword = passwordEncoder.encode(rawPassword);
         user.setPassword(encodedPassword);
         userDao.createUserByUser(user);
         log.info("user = {} ", user);
-        return "redirect:/loginForm";
+        return "redirect:/user/loginForm";
     }
 }
