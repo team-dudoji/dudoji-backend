f236bf8502088a126fd819a8053802b0895b4b02
Vackam <53655740+Vackam@users.noreply.github.com>
2025-03-19 20:32:29 +0900

    feature(PrincipalDetails.java) : security 사용을 위한 클래스 생성

---
+++ src/main/java/com/dudoji/spring/models/domain/PrincipalDetails.java
@@ -0,0 +1,59 @@
+package com.dudoji.spring.models.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private User user;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(
+                new GrantedAuthority() {
+                    @Override
+                    public String getAuthority() {
+                        return "ROLE_" + user.getRole();
+                    }
+                }
+        );
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+}
