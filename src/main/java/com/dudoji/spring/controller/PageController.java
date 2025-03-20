fca34a810d5370199807b58983c375d79956e5e3
Vackam <53655740+Vackam@users.noreply.github.com>
2025-03-20 19:56:35 +0900

    feature(PageController.java) : 주석 처리 및 Deprecated

--- src/main/java/com/dudoji/spring/controller/PageController.java
+++ src/main/java/com/dudoji/spring/controller/PageController.java
@@ -6,19 +6,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Deprecated
 @Controller
 @RequestMapping("/login")
 public class PageController {
-
-    @Value("${kakao.auth.client_id}")
-    private String client_id;
-    @Value("${kakao.auth.redirect_uri}")
-    private String redirect_uri;
-
+//
+//    @Value("${kakao.auth.client_id}")
+//    private String client_id;
+//    @Value("${kakao.auth.redirect_uri}")
+//    private String redirect_uri;
+//
     @GetMapping("/page")
-    public String loginPage(Model model) {
-        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + client_id + "&redirect_uri=" + redirect_uri;
-        model.addAttribute("location", location);
+    public String loginPage() {
         return "login";
     }
 }
