5dc2cf4a3b2563555a655d2778134ad8029e14a9
Vackam <53655740+Vackam@users.noreply.github.com>
2025-04-04 16:08:26 +0900

    feature(DateRequestDto) : 날짜 정보를 보내기 위한 dto

---
+++ src/main/java/com/dudoji/spring/dto/DateRequestDto.java
@@ -0,0 +1,16 @@
+package com.dudoji.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * This Class For Translate Json Body Object to Date
+ */
+@Data
+public class DateRequestDto {
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate step_date;
+}
