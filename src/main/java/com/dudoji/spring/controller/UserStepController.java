cff75096fe59400db196d4977c29a203e858d257
Vackam <53655740+Vackam@users.noreply.github.com>
2025-05-06 16:04:29 +0900

    Revert "feature(UserStepController.java) : ResponseEntity generic 타입으로 변경 및 직렬화 사용"
    This reverts commit caecc3ac8f6c701deb08bb145db5f46370e38310.

--- src/main/java/com/dudoji/spring/controller/UserStepController.java
+++ src/main/java/com/dudoji/spring/controller/UserStepController.java
@@ -49,7 +49,7 @@ public class UserStepController {
      * @return String That Contains Value of UserStep
      */
     @GetMapping("/get/step")
-    public ResponseEntity<?> getUserStep(
+    public ResponseEntity<String> getUserStep(
             @AuthenticationPrincipal PrincipalDetails principal,
             @RequestBody DateRequestDto targetDate
     ) {
@@ -59,8 +59,7 @@ public class UserStepController {
 
         UserStep result = userStepService.getUserStepByIdAndDate(principal.getUid(), targetDate.getStep_date());
 
-        //return ResponseEntity.status(HttpStatus.OK).body(result.toString());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result.toString());
     }
 
     /**
@@ -70,7 +69,7 @@ public class UserStepController {
      * @return String That Contains Value Of UserSteps
      */
     @GetMapping("/get/steps")
-    public ResponseEntity<?> getUserSteps(
+    public ResponseEntity<String> getUserSteps(
             @AuthenticationPrincipal PrincipalDetails principal,
             @RequestBody DateRangeRequestDto targetDuration
     ) {
@@ -79,6 +78,6 @@ public class UserStepController {
         }
 
         List<UserStep> result = userStepService.getUserStepsByIdAndDuration(principal.getUid(), targetDuration.getStartDate().getStep_date(), targetDuration.getEndDate().getStep_date());
-        return ResponseEntity.ok(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result.toString());
     }
 }
