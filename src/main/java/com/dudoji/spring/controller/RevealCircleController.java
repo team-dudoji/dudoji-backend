c96556cbcaddee387636f3d1d9ca75c2d960581b
Yunseong Jeong <88422717+dev-yunseong@users.noreply.github.com>
2025-05-14 13:46:03 +0900

    Update RevealCircleController.java

--- src/main/java/com/dudoji/spring/controller/RevealCircleController.java
+++ src/main/java/com/dudoji/spring/controller/RevealCircleController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/user/reveal-circle")
+@RequestMapping("/api/user/reveal-circles")
 public class RevealCircleController {
 
     @Autowired
