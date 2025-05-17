579f606459d440ab6276cb05a738d22b99bd4b3a
Vackam <53655740+Vackam@users.noreply.github.com>
2025-05-17 14:52:15 +0900

    feature dao : pin, MapSection 테이블 수정 반영 (#54)

--- src/main/java/com/dudoji/spring/models/dao/MapSectionDao.java
+++ src/main/java/com/dudoji/spring/models/dao/MapSectionDao.java
@@ -30,38 +30,38 @@ public class MapSectionDao {
     private static final String GET_MAP_SECTIONS =
             "select m.x, m.y, mb.bitmap " +
             "    from MapSectionStateBitmap as mb right join (\n" +
-            "    select uid, x, y, explored\n" +
+            "    select user_id, x, y, explored\n" +
             "    from MapSection\n" +
-            "    where uid=? and (? <= x and x <= ? and ? <= y and y <= ?)\n" +
+            "    where user_id=? and (? <= x and x <= ? and ? <= y and y <= ?)\n" +
             "    ) as m\n" +
-            "    on (m.uid=mb.uid and m.x=mb.x and m.y =mb.y);";
+            "    on (m.user_id=mb.user_id and m.x=mb.x and m.y =mb.y);";
 
     private static final String GET_MAP_SECTION =
             "select m.x, m.y, mb.bitmap " +
                     "    from MapSectionStateBitmap as mb right join (\n" +
-                    "    select uid, x, y, explored\n" +
+                    "    select user_id, x, y, explored\n" +
                     "    from MapSection\n" +
-                    "    where uid=? and (x=? and y=?)\n" +
+                    "    where user_id=? and (x=? and y=?)\n" +
                     "    ) as m\n" +
-                    "    on (m.uid=mb.uid and m.x=mb.x and m.y =mb.y);";
+                    "    on (m.user_id=mb.user_id and m.x=mb.x and m.y =mb.y);";
 
     private static final String SET_MAP_SECTION =
-            "INSERT INTO MapSection (uid, x, y) " +
+            "INSERT INTO MapSection (user_id, x, y) " +
                     "VALUES (?, ?, ?);";
 
     private static final String SET_MAP_SECTION_BITMAP =
-            "INSERT INTO MapSectionStateBitmap (uid, x, y, bitmap) " +
+            "INSERT INTO MapSectionStateBitmap (user_id, x, y, bitmap) " +
                     "VALUES (?, ?, ?, ?);";
 
     private static final String UPDATE_MAP_SECTION_BITMAP =
             "UPDATE MapSectionStateBitmap " +
                     "SET bitmap=? " +
-                    "WHERE uid=? and X=? and Y=?;";
+                    "WHERE user_id=? and X=? and Y=?;";
 
     private static final String UPDATE_MAP_SECTION =
             "UPDATE MapSection " +
                     "SET explored=TRUE " +
-                    "WHERE uid=? and X=? and Y=?;";
+                    "WHERE user_id=? and X=? and Y=?;";
 
     // Using in MapSectionController
     private static final String GET_USER_MAP_SECTIONS =
@@ -73,19 +73,19 @@ public class MapSectionDao {
                     + "END AS bitmap "
                     + "FROM MapSectionStateBitmap AS mb "
                     + "RIGHT JOIN ( "
-                    + "  SELECT uid, x, y, explored "
+                    + "  SELECT user_id, x, y, explored "
                     + "  FROM MapSection "
-                    + "  WHERE uid = ? "
+                    + "  WHERE user_id = ? "
                     + ") AS m "
-                    + "  ON mb.uid = m.uid "
+                    + "  ON mb.user_id = m.user_id "
                     + "  AND mb.x   = m.x "
                     + "  AND mb.y   = m.y;";
 
-    public List<MapSection> getMapSections(long uid, Point point) {
-        return getMapSections(uid, point, 2);
+    public List<MapSection> getMapSections(long user_id, Point point) {
+        return getMapSections(user_id, point, 2);
     }
 
-    public List<MapSection> getMapSections(long uid, Point point, int radius){
+    public List<MapSection> getMapSections(long user_id, Point point, int radius){
         Pair<Double, Double> googleMapPosition = point.getGoogleMap();
         Pair<Integer, Integer> tileMapPosition = Point.convertGoogleMercatorToTile(googleMapPosition.getX(), point.getGoogleY());
         int maxX = tileMapPosition.getX() + radius;
@@ -96,7 +96,7 @@ public class MapSectionDao {
         try (Connection connection = dbConnection.getConnection()) {
             List<MapSection> mapSections = new ArrayList<>();
             PreparedStatement preparedStatement = connection.prepareStatement(GET_MAP_SECTIONS);
-            preparedStatement.setLong(1, uid);
+            preparedStatement.setLong(1, user_id);
             preparedStatement.setInt(2, minX);
             preparedStatement.setInt(3, maxX);
             preparedStatement.setInt(4, minY);
@@ -108,7 +108,7 @@ public class MapSectionDao {
                 int y = resultSet.getInt("m.y");
                 byte[] bitmap = resultSet.getBytes("mb.bitmap");
                 MapSection mapSection = new MapSection.Builder()
-                        .setUid(uid)
+                        .setUid(user_id)
                         .setX(x)
                         .setY(y)
                         .setBitmap(bitmap)
@@ -121,13 +121,13 @@ public class MapSectionDao {
         }
     }
 
-    public Optional<MapSection> getMapSection(long uid, Point point) {
+    public Optional<MapSection> getMapSection(long user_id, Point point) {
         Pair<Double, Double> googleMapPosition = point.getGoogleMap();
         Pair<Integer, Integer> tileMapPosition = Point.convertGoogleMercatorToTile(googleMapPosition.getX(), point.getGoogleY());
 
         try (Connection connection = dbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(GET_MAP_SECTION);
-            preparedStatement.setLong(1, uid);
+            preparedStatement.setLong(1, user_id);
             preparedStatement.setInt(2, tileMapPosition.getX());
             preparedStatement.setInt(3, tileMapPosition.getY());
             preparedStatement.executeQuery();
@@ -138,7 +138,7 @@ public class MapSectionDao {
                 int y = resultSet.getInt("y");
                 byte[] bitmap = resultSet.getBytes("bitmap");
                 MapSection mapSection = new MapSection.Builder()
-                        .setUid(uid)
+                        .setUid(user_id)
                         .setX(x)
                         .setY(y)
                         .setBitmap(bitmap)
@@ -153,12 +153,12 @@ public class MapSectionDao {
             throw new RuntimeException(e);
         }
     }
-    public Optional<MapSection> getMapSection(long uid, int tileX, int tileY) {
+    public Optional<MapSection> getMapSection(long user_id, int tileX, int tileY) {
         Pair<Integer, Integer> tileMapPosition = new Pair<>(tileX, tileY);
 
         try (Connection connection = dbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(GET_MAP_SECTION);
-            preparedStatement.setLong(1, uid);
+            preparedStatement.setLong(1, user_id);
             preparedStatement.setInt(2, tileMapPosition.getX());
             preparedStatement.setInt(3, tileMapPosition.getY());
             preparedStatement.executeQuery();
@@ -169,7 +169,7 @@ public class MapSectionDao {
                 int y = resultSet.getInt("y");
                 byte[] bitmap = resultSet.getBytes("bitmap");
                 MapSection mapSection = new MapSection.Builder()
-                        .setUid(uid)
+                        .setUid(user_id)
                         .setX(x)
                         .setY(y)
                         .setBitmap(bitmap)
@@ -187,19 +187,19 @@ public class MapSectionDao {
 
     public void createMapSection(MapSection mapSection) {
         if (mapSection instanceof DetailedMapSection detailedMapSection) {
-            long uid = detailedMapSection.getUid();
+            long user_id = detailedMapSection.getUid();
             Pair<Integer, Integer> tileMapPosition = detailedMapSection.getPoint();
 
             try (Connection connection = dbConnection.getConnection()) {
                 PreparedStatement preparedStatement = connection.prepareStatement(SET_MAP_SECTION);
-                preparedStatement.setLong(1, uid);
+                preparedStatement.setLong(1, user_id);
                 preparedStatement.setInt(2, tileMapPosition.getX());
                 preparedStatement.setInt(3, tileMapPosition.getY());
 
                 preparedStatement.executeUpdate();
 
                 PreparedStatement preparedStatement1 = connection.prepareStatement(SET_MAP_SECTION_BITMAP);
-                preparedStatement1.setLong(1, uid);
+                preparedStatement1.setLong(1, user_id);
                 preparedStatement1.setInt(2, tileMapPosition.getX());
                 preparedStatement1.setInt(3, tileMapPosition.getY());
                 preparedStatement1.setBytes(4, ((DetailedMapSection) mapSection).getBitmap());
@@ -214,14 +214,14 @@ public class MapSectionDao {
     public void updateMapSection(MapSection mapSection) {
         if (mapSection instanceof DetailedMapSection detailedMapSection) {
             // detailed 아니면 업데이트 할 이유도 없음
-            long uid = detailedMapSection.getUid();
+            long user_id = detailedMapSection.getUid();
             Pair<Integer, Integer> tileMapPosition = detailedMapSection.getPoint();
             byte[] bitmap = detailedMapSection.getBitmap();
 
             try (Connection connection = dbConnection.getConnection()) {
                 PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_MAP_SECTION_BITMAP);
                 preparedStatement.setBytes(1, bitmap);
-                preparedStatement.setLong(2, uid);
+                preparedStatement.setLong(2, user_id);
                 preparedStatement.setInt(3, tileMapPosition.getX());
                 preparedStatement.setInt(4, tileMapPosition.getY());
                 preparedStatement.executeUpdate();
@@ -229,7 +229,7 @@ public class MapSectionDao {
                 if (BitmapUtil.isExplored(bitmap)) {
                     // TODO: 여기 들어오면 없애는 기능도 만드는 게 필요할 듯
                     PreparedStatement preparedStatement1 = connection.prepareStatement(UPDATE_MAP_SECTION);
-                    preparedStatement1.setLong(1, uid);
+                    preparedStatement1.setLong(1, user_id);
                     preparedStatement1.setInt(2, tileMapPosition.getX());
                     preparedStatement1.setInt(3, tileMapPosition.getY());
                     preparedStatement1.executeUpdate();
@@ -242,12 +242,12 @@ public class MapSectionDao {
     }
 
     // Using In MapSectionController
-    public MapSectionResponseDto getUserMapSections(long uid) {
+    public MapSectionResponseDto getUserMapSections(long user_id) {
         MapSectionResponseDto mapSectionResponseDto = new MapSectionResponseDto();
 
         try (Connection connection = dbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(GET_USER_MAP_SECTIONS);
-            preparedStatement.setLong(1, uid);
+            preparedStatement.setLong(1, user_id);
             preparedStatement.executeQuery();
             ResultSet resultSet = preparedStatement.getResultSet();
             while (resultSet.next()) {
