569ca17d85a5a3c8e4f27525836068b257ea0503
Vackam <53655740+Vackam@users.noreply.github.com>
2025-05-24 15:10:23 +0900

    feature WebConfig 설정

---
+++ src/main/java/com/dudoji/spring/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.dudoji.spring.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${file.upload-dir}") String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/" + uploadDir + "/");  // 절대경로로 지정
+    }
+}
