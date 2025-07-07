aa89eebd01a24807738491dfb67a74b3d43e7be1
Yunseong <dev.yunseong@gmail.com>
2025-07-07 16:52:50 +0900

    Add TODO comments for implementing logic in achievement and quest checkers

--- src/main/java/com/dudoji/spring/service/mission/DailyMovementQuestChecker.java
+++ src/main/java/com/dudoji/spring/service/mission/DailyMovementQuestChecker.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class DailyMovementQuestChecker implements MissionChecker {
     @Override
     public int check(long uid) {
+        // TODO - implement logic
         return 0;
     }
 }
--- src/main/java/com/dudoji/spring/service/mission/NewLandmarkQuestChecker.java
+++ src/main/java/com/dudoji/spring/service/mission/NewLandmarkQuestChecker.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class NewLandmarkQuestChecker implements MissionChecker {
     @Override
     public int check(long uid) {
+        // TODO - implement logic
         return 0;
     }
 }
