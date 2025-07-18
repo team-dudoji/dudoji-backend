76f691d9ad63237553dfe6242008f2b10ebe6ad8
Yunseong <dev.yunseong@gmail.com>
2025-07-18 11:16:57 +0900

    Enhance landmark management: replace image URL input with file upload and preview functionality

--- src/main/resources/static/js/landmarks-management.js
+++ src/main/resources/static/js/landmarks-management.js
@@ -2,6 +2,9 @@
 
 const modal = document.getElementById('modal');
 const modalTitle = document.getElementById('modal-title');
+const modalImagePreview = document.getElementById('modal-imagePreview');
+const modalImageUrl = document.getElementById('modal-imageUrl'); // 서버로부터 받은 최종 이미지 URL을 저장할 hidden input
+const modalImageFile = document.getElementById('modal-imageFile'); // 파일 선택 input
 let modalMode = 'none';
 let selectedLandmarkId = null;
 
@@ -19,8 +22,36 @@ function closeModal() {
     modalMode = 'none';
 }
 
-document.getElementById('infoForm').addEventListener('submit', function(event) {
+document.getElementById('infoForm').addEventListener('submit', async function(event) {
     event.preventDefault();
+
+    const file = modalImageFile.files[0];
+    let finalImageUrl = modalImageUrl.value;
+
+    if (file) { // new Image
+        const imageFormData = new FormData();
+        imageFormData.append("image", file);
+
+        try {
+            const uploadResponse = await fetch('/api/admin/images/landmarks', {
+                method: 'POST',
+                body: imageFormData
+            });
+
+            if (!uploadResponse.ok) {
+                // 예외처리
+                const errorText = await uploadResponse.text();
+                throw new Error(`Failed upload: ${uploadResponse.status} - ${errorText}`);
+            }
+
+            finalImageUrl = await uploadResponse.text();
+        } catch (error) {
+            console.error('이미지 업로드 오류:', error);
+            alert('이미지 업로드 중 오류가 발생했습니다: ' + error.message);
+        }
+    }
+    modalImageUrl.value = finalImageUrl;
+
     if (modalMode === 'add') {
         addLandmark();
     } else if (modalMode === 'update') {
@@ -64,4 +95,13 @@ function sendLandmarkData(url, method) {
     }).then(() => {
         location.reload();
     });
+}
+
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function(){
+        modalImagePreview.src = reader.result;
+        modalImagePreview.style.display = 'block';
+    };
+    reader.readAsDataURL(event.target.files[0]);
 }
