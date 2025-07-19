82dca4947ffad6145a8f68d28fcc0fe149b0cb18
Yunseong <dev.yunseong@gmail.com>
2025-07-19 14:07:14 +0900

    Refactor PinSkinController to remove unused imports and clean up code

--- src/main/java/com/dudoji/spring/controller/skin/PinSkinController.java
+++ src/main/java/com/dudoji/spring/controller/skin/PinSkinController.java
@@ -6,14 +6,12 @@ import com.dudoji.spring.models.domain.PrincipalDetails;
 import com.dudoji.spring.service.skin.PinSkinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
