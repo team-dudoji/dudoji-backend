cd842e448281689b1a6fab78394aafde227fcc07
Yunseong <dev.yunseong@gmail.com>
2025-07-29 10:24:37 +0900

    Enhance request logging to include duration in RequestLogFilter

--- src/main/java/com/dudoji/spring/util/log/RequestLogFilter.java
+++ src/main/java/com/dudoji/spring/util/log/RequestLogFilter.java
@@ -15,15 +15,22 @@ public class RequestLogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         if (servletRequest instanceof HttpServletRequest httpRequest) {
-            log.info(
-                    "Request: method={}, uri={}, ip={}, userAgent={}",
-                    httpRequest.getMethod(),
-                    httpRequest.getRequestURI(),
-                    httpRequest.getRemoteAddr(),
-                    Objects.requireNonNullElse(httpRequest.getHeader(HttpHeaders.USER_AGENT), "Unknown")
-            );
+            long startTime = System.currentTimeMillis();
 
-            filterChain.doFilter(servletRequest, servletResponse);
+            try {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } finally {
+                long duration = System.currentTimeMillis() - startTime;
+
+                log.info(
+                        "Request: method={}, uri={}, ip={}, userAgent={}, duration={}ms",
+                        httpRequest.getMethod(),
+                        httpRequest.getRequestURI(),
+                        httpRequest.getRemoteAddr(),
+                        Objects.requireNonNullElse(httpRequest.getHeader(HttpHeaders.USER_AGENT), "Unknown"),
+                        duration
+                );
+            }
         } else {
             log.warn("Received a non-HTTP request. Skipping request logging.");
             filterChain.doFilter(servletRequest, servletResponse);
